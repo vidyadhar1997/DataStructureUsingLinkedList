4d39a66b34d183acacf9348f1c1948ee655bea7e
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-15 13:26:06 +0530

    [vidyadhar hudge]Add:Welcome message

---
+++ DataStructureUsingLinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructureUsingLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To the Data structure program using Linked List");
+        }
+    }
+}
